b0c47167b613d27efb4b6770a449475fa943a3df
Emre Deveci <23077383+emredeveci@users.noreply.github.com>
2024-02-21 16:56:09 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Leap Year Calculator
+
+- This program asks the user for a year. Then, it determines whether that year is a leap year or not.
+## Authors
+
+- [@emredeveci](https://github.com/emredeveci)
+
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //variable needed
+        int year;
+
+        Scanner scanner = new Scanner(System.in);
+
+        //receive user input
+        System.out.print("Please enter a year: ");
+        year = scanner.nextInt();
+
+        //logic for leap year
+        if (((year % 4 == 00) && (year % 100 != 0)) || (year % 400 == 0)) {
+            System.out.print(year + " is a leap year");
+        } else {
+            System.out.print(year + "is not a leap year");
+        }
+
+        scanner.close();
+    }
+}
